d8acb21d3d405246c443a02e872e07a9f4120a33
Andre <awjrs22@gmail.com>
2019-10-06 17:30:26 -0700

    finished infect_newly_infected person function

--- simulation.py
+++ simulation.py
@@ -173,8 +173,11 @@ class Simulation(object):
         # TODO: Call this method at the end of every time step and infect each Person.
         # TODO: Once you have iterated through the entire list of self.newly_infected, remember
         # to reset self.newly_infected back to an empty list.
-        for item in self.newly_infected:
-            self.newly_infected = []
+        for person in self.population:
+            for _id in self.newly_infected:
+                if person._id == id:
+                    person.infection = self.virus 
+        self.newly_infected = []
 
         pass
 
