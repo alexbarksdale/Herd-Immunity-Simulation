770343eac0e11ac9ba4bae499858033243d60a5e
Andre <awjrs22@gmail.com>
2019-10-10 17:53:18 -0700

    fixed major errors in logger with writing to simulation with help

--- person.py
+++ person.py
@@ -1,123 +1,40 @@
-from random import uniform
 from virus import Virus
+import random
+random.seed(42)
 
 
 class Person(object):
     ''' Person objects will populate the simulation. '''
 
     def __init__(self, _id, is_vaccinated, infection=None):
-        ''' We start out with is_alive = True, because we don't make vampires or zombies.
-        All other values will be set by the simulation when it makes each Person object.
-
-        If person is chosen to be infected when the population is created, the simulation
+        '''
+        We start out with is_alive = True,
+        because we don't make vampires or zombies.
+        All other values will be set by the simulation
+        when it makes each Person object.
+        If person is chosen to be infected when the population is created,
+        the simulation
         should instantiate a Virus object and set it as the value
         self.infection. Otherwise, self.infection should be set to None.
         '''
-        self._id = None  # int
+        self._id = _id  # int
         self.is_alive = True  # boolean
-        self.is_vaccinated = None  # boolean
-        self.infection = None  # Virus object or None
+        self.is_vaccinated = is_vaccinated  # boolean
+        self.infection = infection  # Virus object or None
 
     def did_survive_infection(self):
-        ''' Generate a random number and compare to virus's mortality_rate.
+        '''
+        Generate a random number and compare to virus's mortality_rate.
         If random number is smaller, person dies from the disease.
         If Person survives, they become vaccinated and they have no infection.
         Return a boolean value indicating whether they survived the infection.
         '''
+        immunity_strength = random.random()
+
+        if immunity_strength < self.infection.mortality_rate:
+            self.is_alive = False
+        else:
+            self.is_vaccinated = True
+            self.infection = None
+        return self.is_alive
         # Only called if infection attribute is not None.
-        # TODO:  Finish this method. Should return a Boolean
-        # while self.infection != None:
-        #     num = random.uniform(0, 1)
-        #     if num <= self.infection.mortality_rate:
-        #         return False
-        #     else:
-        #         return True
-        if self.infection:
-            num = uniform(0, 1)
-            if num <= self.infection.mortality_rate:
-                # this person died
-                self.is_alive = False
-                return False
-            else:
-                # this person survives
-                self.is_vaccinated = True
-                return True
-
-
-''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
-def test_vacc_person_instantiation():
-    # create some people to test if our init method works as expected
-    dani = Person(4, True, None)
-    assert dani._id == 4, "it's not 4"
-    assert dani.is_alive == True, "It's not true"
-    assert dani.is_vaccinated == True
-    assert dani.infection == None
-
-    michelle = Person(3, True, None)
-    assert michelle._id == 3
-    assert michelle.is_alive == True
-    assert michelle.is_vaccinated == True
-    assert michelle.infection == None
-
-    dan = Person(6, True, None)
-    assert dan._id == 6 
-    assert dan.is_alive == True
-    assert dan.is_vaccinated == True
-    assert dan.infection == None
-
-    jess = Person(1, True, None)
-    assert jess._id == 1
-    assert jess.is_alive == True
-    assert jess.is_vaccinated == True
-    assert jess.infection == None
-
-    
-
-
-def test_not_vacc_person_instantiation():
-    person = Person(2, False)
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    assert person._id == 2
-    assert person.is_alive == False
-    
-
-
-def test_sick_person_instantiation():
-    # Create a Virus object to give a Person object an infection
-    virus = Virus("Dysentery", 0.7, 0.2)
-    # Create a Person object and give them the virus infection
-    person = Person(3, True, virus)
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    assert person._id == 3
-    assert person.is_alive == True
-    assert person.is_vaccinated == False
-    assert person.infection == virus
-    
-
-
-def test_did_survive_infection():
-    # TODO: Create a Virus object to give a Person object an infection
-    virus = Virus("Dysentery", 0.7, 0.2)
-    # TODO: Create a Person object and give them the virus infection
-    person = Person(4, False, virus)
-
-    # Resolve whether the Person survives the infection or not
-    survived = person.did_survive_infection()
-    # Check if the Person survived or not
-    if survived:
-        assert person.is_alive is True
-        # TODO: Write your own assert statements that test
-        # the values of each attribute for a Person who survived
-        # assert ...
-        assert person.is_vaccinated == True
-    else:
-        assert person.is_alive is False
-        # TODO: Write your own assert statements that test
-        # the values of each attribute for a Person who did not survive
-        # assert ...
-        assert person.is_vaccinated == False
-        
--- virus.py
+++ virus.py
@@ -1,40 +1,58 @@
+import unittest
+
 class Virus(object):
     '''Properties and attributes of the virus used in Simulation.'''
 
     def __init__(self, name, repro_rate, mortality_rate):
+        err_msg = "Rates are too high!"
+        assert repro_rate <= 1 or mortality_rate <= 1, err_msg
         self.name = name
         self.repro_rate = repro_rate
         self.mortality_rate = mortality_rate
 
+class TestVirus(unittest.TestCase):
+    def test_virus_instantiation(self):
+        #TODO: Create your own test that models the virus you are working with
+        '''Check to make sure that the virus instantiator is working.'''
+        virus = Virus("HIV", 0.8, 0.3)
+        assert virus.name == "HIV"
+        assert virus.repro_rate == 0.8
+        assert virus.mortality_rate == 0.3
+        
+        ebola = Virus('ebola', .6, .5)
+        assert ebola.name == "ebola"
+        assert ebola.repro_rate == .6
+        assert ebola.mortality_rate == .5
 
-def test_virus_instantiation():
-    #TODO: Create your own test that models the virus you are working with
-    '''Check to make sure that the virus instantiator is working.'''
-    virus = Virus("HIV", 0.8, 0.3)
-    assert virus.name == "HIV"
-    assert virus.repro_rate == 0.8
-    assert virus.mortality_rate == 0.3
-    
-    ebola = Virus('ebola', .6, .5)
-    assert ebola.name == "ebola"
-    assert ebola.repro_rate == .6
-    assert ebola.mortality_rate == .5
+        Mumps = Virus('Mumps', .2, .7)
+        assert Mumps.name == "Mumps"
+        assert Mumps.repro_rate == .2
+        assert Mumps.mortality_rate == .7
 
-    Mumps = Virus('Mumps', .2, .7)
-    assert Mumps.name == "Mumps"
-    assert Mumps.repro_rate == .2
-    assert Mumps.mortality_rate == .7
+        HIV = Virus('HIV', .8, .8)
+        assert HIV.name == 'HIV'
+        assert HIV.repro_rate == .8
+        assert HIV.mortality_rate == .8
 
-    HIV = Virus('HIV', .8, .8)
-    assert HIV.name == 'HIV'
-    assert HIV.repro_rate == .8
-    assert HIV.mortality_rate == .8
-
-    malaria = Virus('Malaria', .4, .2)
-    assert malaria.name == 'malaria'
-    assert malaria.repro_rate == .4
-    assert malaria.mortality_rate == .2
+        malaria = Virus('Malaria', .4, .2)
+        assert malaria.name == 'malaria'
+        assert malaria.repro_rate == .4
+        assert malaria.mortality_rate == .2
 
 
+    def test_insufficient_args(self):
+        '''
+        Credit to
+        https://stackoverflow.com/questions/88325/how-do-i-unit-test-an-init-method-of-a-python-class-with-assertraises
+        for inspiring this test.
+        '''
+        name = "HIV"
+        repro_rate = 1.2
+        mortality_rate = 2.9
+        self.assertRaises(AssertionError,
+                          Virus, name, repro_rate, mortality_rate)
 
-    
+
+
+if __name__ == '__main__':
+    unittest.main()
